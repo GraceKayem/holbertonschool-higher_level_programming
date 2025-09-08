dacc531ba973d09fa08310c6c13e0367c6818cb2
GraceKayem <11557@holbertonstudents.com>
2025-09-09 00:48:12 +1000

    save

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for number in range(0, 10):
-  for reverse_digit in range(number + 1, 10):
-    if number == 8 and reverse_digit == 9:
-      print("{}{}".format(number, reverse_digit))
-    else:
-      print("{}{}".format(number, reverse_digit), end=", ")
+    for reverse_digit in range(number + 1, 10):
+        if number == 8 and reverse_digit == 9:
+            print("{}{}".format(number, reverse_digit))
+        else:
+            print("{}{}".format(number, reverse_digit), end=", ")
