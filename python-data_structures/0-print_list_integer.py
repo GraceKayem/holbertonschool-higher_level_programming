31985e0ef84da4f254e7e8ff4dea54c0c8b2bc98
GraceKayem <11557@holbertonstudents.com>
2025-09-14 21:37:15 +1000

    save

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
 
-  print_list = my_list[0:5]
-  for i in print_list:
-    print("{}".format(my_list) my_list, end="")
+    for i in my_list:
+        print("{}".format(i))
