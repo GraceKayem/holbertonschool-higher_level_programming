6ef06d88b9cd37d4629363d2eecd63d6b2557243
GraceKayem <11557@holbertonstudents.com>
2025-09-20 11:36:05 +1000

    save

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-
     counts = []
-    #counting the occurrences of each number
     for add in my_list:
         if add not in counts:
             counts.append(add)
