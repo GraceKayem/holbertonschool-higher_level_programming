4f37caceb59bf736c6b20b8593c46938ef515298
GraceKayem <11557@holbertonstudents.com>
2025-09-20 14:02:34 +1000

    save

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-
     if not isinstance(roman_string, str) or not roman_string:
         return 0
 
@@ -9,10 +8,8 @@ def roman_to_int(roman_string):
 
     res = 0
     i = 0
-    
     while i < len(roman_string):
         s1 = values[roman_string[i]]
-
         if i + 1 < len(roman_string):
             s2 = values[roman_string[i + 1]]
             if s1 >= s2:
@@ -24,5 +21,4 @@ def roman_to_int(roman_string):
         else:
             res += s1
             i += 1
-
     return res
