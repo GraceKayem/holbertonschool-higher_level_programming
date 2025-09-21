15523fa2a0239541973a915788f21cb7b7b3d61e
GraceKayem <11557@holbertonstudents.com>
2025-09-21 22:40:52 +1000

    save

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -57,5 +57,5 @@ class Rectangle:
         return "\n".join(["#" * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
-        """Return a string representation that can recreate the rectangle."""
-        return f"Rectangle({self.__width}, {self.__height})"
+        """Return the official string representation of the Rectangle."""
+        return f"<{self.__class__.__module__}.{self.__class__.__name__} object at {hex(id(self))}>"
