fa62051b17304d03446375b4a12f2f0dddac2f16
GraceKayem <11557@holbertonstudents.com>
2025-09-22 00:18:05 +1000

    save

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-Add two integers a and b
+Module that defines a function add_integer(a, b=98)
+which adds two numbers (integers or floats) after rounding them.
 """
 
 def add_integer(a, b=98):
     """
-    Add two integers a and b.
+    Add two integers or floats after rounding them.
 
     Args:
         a (int or float): first number
@@ -13,20 +14,13 @@ def add_integer(a, b=98):
 
     Raises:
         TypeError: if a or b is not an int or float
-        OverflowError: if float too large to convert to int
 
     Returns:
-        int: the sum of a and b
+        int: the sum of a and b after rounding
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    try:
-        a_int = int(a)
-        b_int = int(b)
-    except OverflowError:
-        raise OverflowError("float too large to convert to int")
-
-    return a_int + b_int
+    return round(a) + round(b)
