5fa2d0948912ac5d19a9799bcac60d72b8e4aa65
GraceKayem <11557@holbertonstudents.com>
2025-09-22 03:07:38 +1000

    save

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -2,7 +2,8 @@
 """
 2-matrix_divided module.
 
-This module contains a function that divides all elements of a matrix by a given number.
+This module contains a function that divides all elements of a matrix
+by a given number.
 """
 
 
@@ -28,14 +29,20 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    if (not isinstance(matrix, list) or
-        any(not isinstance(row, list) for row in matrix) or
-        any(not all(isinstance(el, (int, float)) for el in row) for row in matrix)):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if (not isinstance(matrix, list)
+            or any(not isinstance(row, list) for row in matrix)
+            or any(not all(isinstance(el, (int, float)) for el in row)
+                   for row in matrix)):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
 
     row_lengths = [len(row) for row in matrix]
     if len(set(row_lengths)) != 1:
         raise TypeError("Each row of the matrix must have the same size")
 
-    new_matrix = [[round(el / div, 2) for el in row] for row in matrix]
+    new_matrix = [
+        [round(el / div, 2) for el in row]
+        for row in matrix
+    ]
     return new_matrix
