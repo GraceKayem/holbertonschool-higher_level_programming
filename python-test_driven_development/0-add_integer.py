955a1c2c49e5508b4e811098a7fcf4532b4dbe2a
GraceKayem <11557@holbertonstudents.com>
2025-09-21 23:57:26 +1000

    save

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
 """
-    Add two integers a and b 
+Add two integers a and b
 """
 
 def add_integer(a, b=98):
     """
-    Add two integers a and b 
+    Add two integers a and b.
 
     Args:
-        a (int or float): num 1
-        b (int, float or optional): num 2 - defaults to 98.
+        a (int or float): first number
+        b (int or float, optional): second number, defaults to 98
 
     Raises:
-        TypeError: a must be an integer or b must be an integer
+        TypeError: if a or b is not an int or float
 
     Returns:
-        int: a plus b
+        int: the sum of a and b
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
-    return int(a) + int(b)
+
+    try:
+        a_int = int(a)
+        b_int = int(b)
+    except OverflowError:
+        raise OverflowError("float too large to convert to int")
+
+    return a_int + b_int
