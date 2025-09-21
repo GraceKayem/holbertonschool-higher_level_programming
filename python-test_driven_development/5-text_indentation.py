6d250ef7c416ecee17c460ae5c977d2bec6b7ca7
GraceKayem <11557@holbertonstudents.com>
2025-09-22 02:50:31 +1000

    save

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -8,18 +8,14 @@ two new lines after each '.', '?', and ':' character.
 
 
 def text_indentation(text):
+    """Print text with 2 new lines after '.', '?', or ':'"""
+
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    end_chars = ".?:"
-    start = 0
+    for char in ".:?":
+        text = text.replace(char, "{}\n".format(char))
 
-    for index, char in enumerate(text):
-        if char in end_chars:
-            print(text[start:index + 1].strip())
-            print()
-            start = index + 1
-
-    remaining = text[start:].strip()
-    if remaining:
-        print(remaining)
+    lines = text.splitlines()
+    for i, line in enumerate(lines):
+        print(line.strip(), end='' if i == len(lines) - 1 else '\n\n')
