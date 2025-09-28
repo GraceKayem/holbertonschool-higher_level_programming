5164ce8286141fc99b48e2b241c3077ddd452771
GraceKayem <11557@holbertonstudents.com>
2025-09-28 19:53:21 +1000

    save

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Return True if obj is an instance of a subclass (direct or indirect) 
+Return True if obj is an instance of a subclass (direct or indirect)
 of a_class; otherwise False.
 """
 
@@ -10,7 +10,7 @@ def inherits_from(obj, a_class):
     Return True if obj is an instance of a subclass (direct or indirect)
     of a_class; otherwise False.
     """
-    if isinstance(obj, a_class) and type(obj) != a_class:
+    if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
     else:
         return False
