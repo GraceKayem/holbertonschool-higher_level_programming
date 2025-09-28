6abe78005b5cf7276b34c088aff54160bed04050
GraceKayem <11557@holbertonstudents.com>
2025-09-28 23:13:59 +1000

    save

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -3,28 +3,28 @@
 In some object-oriented languages, a class can inherit attributes and behaviors from more than one parent class.
 """
 
-
 class Fish:
-    """Create a Fish class with methods swim (which prints “The fish is swimming”) and habitat (which prints “The fish lives in water”)."""
+    """Fish class with swim and habitat methods."""
     def swim(self):
         print("The fish is swimming")
 
     def habitat(self):
         print("The fish lives in water")
 
+
 class Bird:
-    """Create a Bird class with methods fly (which prints “The bird is flying”) and habitat (which prints “The bird lives in the sky”).
-    """
+    """Bird class with fly and habitat methods."""
     def fly(self):
         print("The bird is flying")
 
     def habitat(self):
         print("The bird lives in the sky")
 
+
 class FlyingFish(Fish, Bird):
-    """Implementing FlyingFish"""
+    """FlyingFish class demonstrating multiple inheritance."""
     def fly(self):
-        print("The flying fish is soaring!.")
+        print("The flying fish is soaring!")
 
     def swim(self):
         print("The flying fish is swimming!")
