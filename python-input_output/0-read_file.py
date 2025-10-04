e512100fe91864b82bd616d853182a64134a6970
GraceKayem <11557@holbertonstudents.com>
2025-10-05 01:19:35 +1000

    save

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Write a function that reads a text file (UTF8) and prints it to stdout:
+Write a function that reads a text file (UTF8) and prints it to stdout.
 """
 
 
 def read_file(filename=""):
     """
     Reads a text file (UTF8) and prints its content to stdout.
-    
+
     Args:
         filename (str): The name of the file to read. Defaults to "".
     """
