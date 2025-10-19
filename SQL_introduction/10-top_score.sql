bfa7f9b4aff4c74ad5935553ab80313751c95a80
GraceKayem <11557@holbertonstudents.com>
2025-10-19 10:16:50 +0000

    save

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,7 @@
 -- script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
-SHOW COUNT(*) CREATE TABLE second_table;
+USE hbtn_0c_0;
+
+SELECT score, name 
+FROM second_table
+ORDER BY score ASC;
 
