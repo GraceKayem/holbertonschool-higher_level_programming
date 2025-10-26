dbf2b6430be9acfc42bacae1504631e72bd7e075
GraceKayem <11557@holbertonstudents.com>
2025-10-26 10:43:33 +0000

    save

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server
-
+USE hbtn_0d_tvshows;
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
