dcb120b7e22cefd63eb9d91d1ec4d10f85d19485
GraceKayem <11557@holbertonstudents.com>
2025-10-26 11:46:52 +0000

    save

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -5,5 +5,5 @@ FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.show_id IS NULL
-ORDER BY tv_shows.title, tv_shows_genres.genre_id;
+ORDER BY tv_shows.title;
 
