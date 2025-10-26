63bd82bcb884bd27cbdcdfec9445cf0ac29dc8cb
GraceKayem <11557@holbertonstudents.com>
2025-10-26 10:02:41 +0000

    save

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,7 +3,8 @@ USE hbtn_0d_usa;
 -- The states table contains only one record where name = California (but the id can be different, as per the example)
 
 -- selecting id and name columns from the cities table
-SELECT * FROM cities
+SELECT id, name 
+FROM cities
 -- include whose state_id matches one of the IDs
 WHERE state_id IN (
 	-- subquery
