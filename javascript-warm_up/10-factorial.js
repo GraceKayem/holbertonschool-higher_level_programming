d40b0ce2bab87b5a331fe7374005cccf600ec3d8
GraceKayem <11557@holbertonstudents.com>
2025-11-09 23:25:04 +1100

    save

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/*script that computes and prints a factorial*/
+function factorial(n) {
+  const num = parseInt(n);
+  if (isNaN(num) || num <= 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+const arg = process.argv[2];
+
+console.log(factorial(arg));
---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/*script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer*/
+const arg = process.argv[2];
+const num = parseInt(arg);
+
+if (Number.isNaN(num)) {
+  console.log("Not a number");
+} else {
+  console.log(`My number: ${num}`);
+}
