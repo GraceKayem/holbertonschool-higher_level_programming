e1b2152dff5c3508e6522956c9fc402b16498796
GraceKayem <11557@holbertonstudents.com>
2025-11-15 20:11:26 +1100

    save

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,37 +1,34 @@
 #!/usr/bin/python3
 """
-Script that deletes all State objects with a name containing 'a'
-from the database hbtn_0e_6_usa using SQLAlchemy ORM.
+Module to perform simple queries on the model_state model
+using an ORM - SQLAlchemy.
 """
-
-import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+import sys
 
 
-def delete_states_with_a(user: str, passwd: str, db_name: str) -> None:
-    """Connect to the database and delete states containing 'a' in their name."""
+def delete_states_with_a(user: str, passwd: str, dbname: str) -> None:
+    """Delete all State objects containing 'a' in their name."""
     session = None
     try:
         engine = create_engine(
-            "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user, passwd, db_name),
+            "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user, passwd, dbname),
             pool_pre_ping=True
         )
         Session = sessionmaker(bind=engine)
         session = Session()
 
-        # Query all states containing 'a'
-        states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
-
-        # Delete each state
-        for state in states_to_delete:
+        states = session.query(State).filter(State.name.like('%a%')).all()
+        for state in states:
             session.delete(state)
 
         session.commit()
 
     except Exception as e:
         print(e)
+
     finally:
         if session:
             session.close()
@@ -39,9 +36,7 @@ def delete_states_with_a(user: str, passwd: str, db_name: str) -> None:
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print(
-            "Usage: {} <mysql_user> <mysql_password> <db_name>".format(sys.argv[0])
-        )
+        print("Usage: {} <mysql_user> <mysql_password> <db_name>".format(sys.argv[0]))
         sys.exit(1)
 
     delete_states_with_a(sys.argv[1], sys.argv[2], sys.argv[3])
