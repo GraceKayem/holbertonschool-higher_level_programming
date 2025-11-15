f0d9931a71e94a5389f15a3dda9550b604e36149
GraceKayem <11557@holbertonstudents.com>
2025-11-15 22:56:19 +1100

    save

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
 """
-Module to perform simple queries on the model_state model
-using an ORM - SQLAlchemy.
+Script that deletes all State objects with a name containing the letter 'a'
+from the database hbtn_0e_6_usa.
+
+- Uses SQLAlchemy ORM
+- Imports Base and State from model_state
+- Connects to a MySQL server running on localhost at port 3306
+- Does not execute when imported
 """
-from model_state import Base, State
+
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sys
+from model_state import Base, State
 
 
 def delete_states_with_a(user: str, passwd: str, dbname: str) -> None:
     """Delete all State objects containing 'a' in their name."""
     session = None
     try:
+        # Create engine and session
         engine = create_engine(
-            "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user, passwd, dbname),
+            f"mysql+mysqldb://{user}:{passwd}@localhost:3306/{dbname}",
             pool_pre_ping=True
         )
         Session = sessionmaker(bind=engine)
         session = Session()
 
-        states = session.query(State).filter(State.name.like('%a%')).all()
-        for state in states:
+        # Find and delete states with 'a' in the name
+        states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+        for state in states_to_delete:
             session.delete(state)
 
         session.commit()
@@ -36,7 +44,7 @@ def delete_states_with_a(user: str, passwd: str, dbname: str) -> None:
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} <mysql_user> <mysql_password> <db_name>".format(sys.argv[0]))
+        print(f"Usage: {sys.argv[0]} <mysql_user> <mysql_password> <db_name>")
         sys.exit(1)
 
     delete_states_with_a(sys.argv[1], sys.argv[2], sys.argv[3])
