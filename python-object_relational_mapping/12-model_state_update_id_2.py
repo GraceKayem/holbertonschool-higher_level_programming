86ac193eb1f5286fc6f94f935d3ca8ac35d0823f
GraceKayem <11557@holbertonstudents.com>
2025-11-15 22:05:29 +1100

    save

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
-"""Updates the State object with id=2 to 'New Mexico'"""
 
-import sys
+"""
+Module to perfom simple queries on the model_state model
+using and ORM - SQLAlchemy
+"""
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+import sys
+
+
+def connect_and_query(user: str, passwd: str, dbase: str) -> None:
+
+    """
+    Connect to the database and make queries using ORM
+    """
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                               .format(user, passwd, dbase))
+        Session = sessionmaker(bind=engine)
+        state_session = Session()
+        state = state_session.query(State).filter_by(id=2).first()
+        state.name = "New Mexico"
+        state_session.commit()
+
+    except Exception as e:
+        return e
+
 
 if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    state = session.query(State).get(2)
-    if state:
-        state.name = "New Mexico"
-        session.commit()
+    connect_and_query(sys.argv[1], sys.argv[2], sys.argv[3])
--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
+"""Deletes all State objects containing the letter 'a'"""
 
-"""
-Module to perfom simple queries on the model_state model
-using and ORM - SQLAlchemy
-"""
-from model_state import Base, State
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sys
-
-
-def connect_and_query(user: str, passwd: str, dbase: str) -> None:
-
-    """
-    Connect to the database and make queries using ORM
-    """
-    try:
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                               .format(user, passwd, dbase))
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        states = session.query(State).filter(State.name.like('%a%')).all()
-        [session.delete(state) for state in states]
-        session.commit()
-
-    except Exception as e:
-        return e
-
+from model_state import Base, State
 
 if __name__ == "__main__":
-    connect_and_query(sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+    for state in states_to_delete:
+        session.delete(state)
+    session.commit()
