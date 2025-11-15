0ee5748b6116fd1ebddce24f83deace2f9efa37e
GraceKayem <11557@holbertonstudents.com>
2025-11-15 20:07:07 +1100

    save

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 """
-Script that deletes all State objects with a name containing the letter 'a'
-from the database hbtn_0e_6_usa.
-
-- Uses SQLAlchemy ORM.
-- Imports Base and State from model_state.
-- Connects to a MySQL server running on localhost at port 3306.
-- Does not execute when imported.
+Script that deletes all State objects with a name containing 'a'
+from the database hbtn_0e_6_usa using SQLAlchemy ORM.
 """
 
 import sys
@@ -15,29 +10,31 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 
-def delete_states_with_a(mysql_user, mysql_pass, db_name):
-    """
-    Connects to the database and deletes all State objects
-    where the name contains the letter 'a'.
-    """
-    engine = create_engine(
-        f"mysql+mysqldb://{mysql_user}:{mysql_pass}@localhost:3306/{db_name}",
-        pool_pre_ping=True
-    )
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
+def delete_states_with_a(user: str, passwd: str, db_name: str) -> None:
+    """Connect to the database and delete states containing 'a' in their name."""
+    session = None
     try:
+        engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user, passwd, db_name),
+            pool_pre_ping=True
+        )
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
         # Query all states containing 'a'
         states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
 
+        # Delete each state
         for state in states_to_delete:
             session.delete(state)
 
         session.commit()
+
+    except Exception as e:
+        print(e)
     finally:
-        session.close()
+        if session:
+            session.close()
 
 
 if __name__ == "__main__":
@@ -47,8 +44,4 @@ if __name__ == "__main__":
         )
         sys.exit(1)
 
-    user = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    delete_states_with_a(user, password, db_name)
+    delete_states_with_a(sys.argv[1], sys.argv[2], sys.argv[3])
