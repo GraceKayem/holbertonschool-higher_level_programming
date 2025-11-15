4751d451031d42ed87cd5720852ad60174f1e826
GraceKayem <11557@holbertonstudents.com>
2025-11-15 19:37:04 +1100

    save

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,50 +1,36 @@
 #!/usr/bin/python3
 """
-Script that adds the State object "Louisiana" to the database hbtn_0e_6_usa.
-
-- Uses SQLAlchemy ORM.
-- Imports Base and State from model_state.
-- Connects to a MySQL server running on localhost at port 3306.
-- Prints the id of the new State after creation.
-- Does not execute when imported.
+Module to perform simple queries on the model_state model
+using an ORM - SQLAlchemy
 """
-
-import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+import sys
 
 
-def add_state(mysql_user, mysql_pass, db_name):
+def connect_and_query(user: str, passwd: str, dbase: str) -> None:
     """
-    Connect to the database, add a new State named "Louisiana",
-    and print its id.
+    Connect to the database and add a State named "Louisiana"
     """
     session = None
     try:
-        # Create SQLAlchemy engine
         engine = create_engine(
-            "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-                mysql_user, mysql_pass, db_name
-            ),
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(user, passwd, dbase),
             pool_pre_ping=True
         )
-
-        # Create a configured "Session" class
         Session = sessionmaker(bind=engine)
         session = Session()
 
-        # Create and add a new State object
-        new_state = State(name="Louisiana")
-        session.add(new_state)
+        # Create and add Louisiana
+        louisiana = State(name="Louisiana")
+        session.add(louisiana)
         session.commit()
 
         # Print the id of the new state
-        print(new_state.id)
-
+        print(louisiana.id)
     except Exception as e:
         print(e)
-
     finally:
         if session:
             session.close()
@@ -53,10 +39,8 @@ def add_state(mysql_user, mysql_pass, db_name):
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print(
-            "Usage: {} <mysql_user> <mysql_password> <db_name>".format(
-                sys.argv[0]
-            )
+            "Usage: {} <mysql_user> <mysql_password> <db_name>".format(sys.argv[0])
         )
         sys.exit(1)
 
-    add_state(sys.argv[1], sys.argv[2], sys.argv[3])
+    connect_and_query(sys.argv[1], sys.argv[2], sys.argv[3])
