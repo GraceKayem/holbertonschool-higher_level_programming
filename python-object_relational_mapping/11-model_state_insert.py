1638edc8094643f1c25d22dd2789cf4c59e01cb9
GraceKayem <11557@holbertonstudents.com>
2025-11-15 19:44:18 +1100

    save

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,37 +1,43 @@
 #!/usr/bin/python3
 """
-Module to add the State object "Louisiana" to the database hbtn_0e_6_usa
-using SQLAlchemy ORM.
+Script that adds the State object "Louisiana" to the database hbtn_0e_6_usa.
+
+- Uses SQLAlchemy ORM
+- Imports Base and State from model_state
+- Connects to a MySQL server running on localhost at port 3306
+- Prints the new state's id after creation
+- Does not execute when imported
 """
-from model_state import Base, State
+
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sys
+from model_state import Base, State
 
 
-def connect_and_query(user: str, passwd: str, dbase: str) -> None:
-    """
-    Connect to the database, add a new State named "Louisiana",
-    and print its id.
-    """
+def add_louisiana(user: str, passwd: str, db_name: str) -> None:
+    """Connect to the database, add Louisiana, and print its id."""
     session = None
     try:
+        # Connect to MySQL database
         engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(user, passwd, dbase),
+            "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user, passwd, db_name),
             pool_pre_ping=True
         )
         Session = sessionmaker(bind=engine)
         session = Session()
 
-        # Add Louisiana
+        # Create and add Louisiana
         louisiana = State(name="Louisiana")
         session.add(louisiana)
         session.commit()
 
-        # Print the id of the new State
+        # Print the id of the new state
         print(louisiana.id)
+
     except Exception as e:
         print(e)
+
     finally:
         if session:
             session.close()
@@ -39,9 +45,7 @@ def connect_and_query(user: str, passwd: str, dbase: str) -> None:
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print(
-            "Usage: {} <mysql_user> <mysql_password> <db_name>".format(sys.argv[0])
-        )
+        print("Usage: {} <mysql_user> <mysql_password> <db_name>".format(sys.argv[0]))
         sys.exit(1)
 
-    connect_and_query(sys.argv[1], sys.argv[2], sys.argv[3])
+    add_louisiana(sys.argv[1], sys.argv[2], sys.argv[3])
