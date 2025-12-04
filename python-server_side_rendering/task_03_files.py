32e8828b34769225b12f2ba6276bde0dc53f710b
GraceKayem <11557@holbertonstudents.com>
2025-12-04 23:25:50 +1100

    Displaying Data from JSON or CSV Files in Flask

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template, request
+import json
+import csv
+
+app = Flask(__name__)
+
+def json_file():
+    try:
+        with open("products.json", "r") as f:
+            return json.load(f).get("products", [])
+    except Exception:
+        return None
+
+def csv_file():
+    products = []
+    try:
+        with open("products.csv", newline="") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                products.append({
+                    "id": int(row["id"]),
+                    "name": row["name"],
+                    "category": row["category"],
+                    "price": float(row["price"])
+                })
+        return products
+    except Exception:
+        return None
+
+@app.route('/products')
+def products():
+    source = request.args.get("source")
+    product_id = request.args.get("id")
+
+
+    if source == "json":
+        data = json_file()
+    elif source == "csv":
+        data = csv_file()
+    else:
+        return render_template("product_display.html", error="Wrong source")
+
+
+    if data is None:
+        return render_template("product_display.html", error="Could not read the file")
+
+
+    if product_id:
+        try:
+            product_id = int(product_id)
+        except ValueError:
+            return render_template("product_display.html", error="Invalid id")
+
+        filtered = [p for p in data if p["id"] == product_id]
+
+        if not filtered:
+            return render_template("product_display.html", error="Product not found")
+
+        return render_template("product_display.html", products=filtered)
+
+
+    return render_template("product_display.html", products=data)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
